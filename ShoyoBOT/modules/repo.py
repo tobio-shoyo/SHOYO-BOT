2fa0e823ecd9c10754794409b3d8c85477d5acf3
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2021-10-22 22:52:57 +0530

    Add files via upload

---
+++ ShoyoBOT/modules/repo.py
@@ -0,0 +1,28 @@
+from ShoyoBOT import dispatcher
+
+from telegram import ParseMode, Update
+from telegram.ext import CommandHandler, CallbackContext
+from telegram.ext.dispatcher import  run_async
+
+GIT_PIC = "https://telegra.ph/file/311df2003dc985a39ddf6.jpg"
+
+GIT_TEXT = """
+Hinata Shōyō Robot By @INTERVIER_RRRR
+
+*Contributors/Credits*
+
+> [IzumiCypherX](https://github.com/tobio-shoyo)
+
+"""
+
+@run_async
+def repo(update: Update, context: CallbackContext):
+    update.effective_message.reply_photo(
+        GIT_PIC,
+        caption = GIT_TEXT,
+        parse_mode=ParseMode.MARKDOWN
+        )
+
+REPO_HANDLER = CommandHandler("repo", repo)
+
+dispatcher.add_handler(REPO_HANDLER)
